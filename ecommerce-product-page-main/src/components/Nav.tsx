87d2772b6c731634848445a8313186a25dfbe450
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-07-28 15:11:26 +0300

    Add nav underline

--- ecommerce-product-page-main/src/components/Nav.tsx
+++ ecommerce-product-page-main/src/components/Nav.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-
 const Nav = () => {
+  const navItemStyle = `font-sans cursor-pointer hover:-mb-8 hover:border-b-2 hover:border-orange-400`;
   return (
     <nav className="flex flex-row space-x-8 text-gray-500">
-      <p className="font-sans cursor-pointer">Collections</p>
-      <p className="font-sans cursor-pointer">Men</p>
-      <p className="font-sans cursor-pointer">Women</p>
-      <p className="font-sans cursor-pointer">About</p>
-      <p className="font-sans cursor-pointer">Contact</p>
+      <p className={navItemStyle}>Collections</p>
+      <p className={navItemStyle}>Men</p>
+      <p className={navItemStyle}>Women</p>
+      <p className={navItemStyle}>About</p>
+      <p className={navItemStyle}>Contact</p>
     </nav>
   );
 };
