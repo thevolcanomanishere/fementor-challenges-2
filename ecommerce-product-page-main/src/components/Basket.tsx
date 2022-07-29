13154b2c8298112d3fb177849bacc1ea230b3e81
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-07-29 11:52:48 +0300

    Add show basket animation

--- ecommerce-product-page-main/src/components/Basket.tsx
+++ ecommerce-product-page-main/src/components/Basket.tsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ReactComponent as IconBasket } from "../../public/icon-cart.svg";
 import CartContext from "../contexts/CartContext";
 
@@ -6,6 +7,11 @@ export const Basket = () => {
   const [showBasket, setShowBasket] = useState(false);
   const { cart, setCart } = useContext(CartContext);
   const [cartBounce, setCartBounce] = useState(false);
+  const parent = useRef(null);
+
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current, { duration: 100 });
+  }, [parent]);
 
   const basketStyle = showBasket
     ? "flex flex-col justify-start w-[20rem] absolute top-11 -right-36 border rounded-lg bg-white shadow-xl mr-10"
@@ -25,34 +31,36 @@ export const Basket = () => {
   }, [cart]);
 
   return (
-    <div className="m-auto relative">
+    <div ref={parent} className="m-auto relative">
       <div
         className={cartBounceStyle}
         onClick={() => setShowBasket(!showBasket)}
       >
         <IconBasket />
       </div>
-      <section className={basketStyle}>
-        <div className="flex flex-row justify-between">
-          <h1 className="font-semibold p-5">Cart</h1>
-          <div
-            onClick={() => setShowBasket(false)}
-            className="text-xl p-5 cursor-pointer"
-          >
-            X
+      {showBasket && (
+        <section className={basketStyle}>
+          <div className="flex flex-row justify-between">
+            <h1 className="font-semibold p-5">Cart</h1>
+            <div
+              onClick={() => setShowBasket(false)}
+              className="text-xl p-5 cursor-pointer"
+            >
+              X
+            </div>
           </div>
-        </div>
-        <div className="border-b" />
-        <div className="flex min-h-fit">
-          {cart?.length ? (
-            <h1>{cart.length}</h1>
-          ) : (
-            <h2 className="m-auto p-20 text-gray-500 font-semibold">
-              Your cart is empty.
-            </h2>
-          )}
-        </div>
-      </section>
+          <div className="border-b" />
+          <div className="flex min-h-fit">
+            {cart?.length ? (
+              <h1>{cart.length}</h1>
+            ) : (
+              <h2 className="m-auto p-20 text-gray-500 font-semibold">
+                Your cart is empty.
+              </h2>
+            )}
+          </div>
+        </section>
+      )}
     </div>
   );
 };
