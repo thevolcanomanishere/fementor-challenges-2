5f723a9dbe791d5b6b5f4faa591dc42a95658c2a
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-07-29 19:59:21 +0300

    Add qrcode

---
+++ qr-code-component-main/src/App.tsx
@@ -0,0 +1,34 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+const App = () => {
+  return (
+    <main
+      className="flex justify-center items-center h-screen bg-gradient-to-r
+    from-blue-200
+    via-white
+    to-qrBlue
+    background-animate"
+    >
+      <div className="text-center max-w-sm m-5 border rounded-2xl bg-white">
+        <div className="flex flex-col shadow-xl rounded-2xl">
+          <img
+            className="rounded-xl m-4"
+            src="/image-qr-code.png"
+            alt="QR code"
+          />
+          <div className="flex flex-col space-y-3 px-10 py-5">
+            <h1 className="font-bold text-2xl">
+              Improve your front-end skills by building projects
+            </h1>
+            <p className="text-lg text-gray-400 pb-4">
+              Scan the QR code to visit Frontend Mento and take your coding
+              skils to the next level
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ qr-code-component-main/tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        scoreColor: "#6061A1",
+        qrBlue: "hsl(218, 44%, 22%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        liteGray: "hsl(212, 45%, 89%)",
+      },
+      animation: {
+        // Bounces for a total of 5 seconds
+        "bounce-short": "bounce 1s ease-in-out 5",
+      },
+    },
+  },
+  plugins: [],
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
+  },
+};
