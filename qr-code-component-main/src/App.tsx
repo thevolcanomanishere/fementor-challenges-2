b1550c5b91c47d8f99d5d580ada7f71a6dd399db
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-07-29 20:13:00 +0300

    Change size

--- qr-code-component-main/src/App.tsx
+++ qr-code-component-main/src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
     to-qrBlue
     background-animate"
     >
-      <div className="text-center max-w-sm m-5 border rounded-2xl bg-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="text-center max-w-xs m-5 border rounded-2xl bg-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-300">
         <div className="flex flex-col shadow-xl rounded-2xl">
           <img
             className="rounded-xl m-4"
@@ -17,10 +17,10 @@ const App = () => {
             alt="QR code"
           />
           <div className="flex flex-col space-y-3 px-10 py-5">
-            <h1 className="font-bold text-2xl">
+            <h1 className="font-bold text-xl">
               Improve your front-end skills by building projects
             </h1>
-            <p className="text-lg text-gray-400 pb-4">
+            <p className="text-md text-gray-400 pb-4">
               Scan the QR code to visit Frontend Mentor and take your coding
               skils to the next level
             </p>
